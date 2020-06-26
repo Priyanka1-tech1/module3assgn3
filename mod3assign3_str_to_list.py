86838c7cdc7bc630e92d60d393af516a70975562
Priyanka1-tech1 <67084288+Priyanka1-tech1@users.noreply.github.com>
2020-06-26 20:34:28 +0530

    Add files via upload

---
+++ mod3assign3_str_to_list.py
@@ -0,0 +1,5 @@
+def Convert(string): 
+    li = list(string.split("-")) 
+    return li     
+str1 = "python-java-c++"
+print(Convert(str1)) 
---
+++ mod3assign3_sub_string.py
@@ -0,0 +1,4 @@
+str1 = 'python is an interpreted language and python is a object oriented programming'
+print()
+print(str1.count("python"))
+print()
